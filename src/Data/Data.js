68cd612741aa95b8f51897bec93be89ee5fdb46d
dasabhinaba34 <dasabhinaba34@gmail.com>
2023-02-08 01:51:32 +0530

    updates part updated

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -10,6 +10,10 @@ import {
     UilMoneyWithdrawal,
   } from "@iconscout/react-unicons";
 
+  import img1 from '../imgs/img1.png'
+  import img2 from '../imgs/img2.png'
+  import img3 from '../imgs/img3.png'
+
 // Sidebar Data
 export const SidebarData = [
     {
@@ -87,3 +91,26 @@ export const CardsData = [
         ],
     },
 ]
+
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Andrew Thomas",
+    noti: "has ordered Apple smart watch 2500mh battery.",
+    time: "25 seconds ago",
+  },
+  {
+    img: img2,
+    name: "James Bond",
+    noti: "has received Samsung gadget for charging battery.",
+    time: "30 minutes ago",
+  },
+  {
+    img: img3,
+    name: "Iron Man",
+    noti: "has ordered Apple smart watch, samsung Gear 2500mh battery.",
+    time: "2 hours ago",
+  },
+];
